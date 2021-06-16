fee1eb230cbc6b7d097e69768b97e95ea9fef9b5
Jeferson Oliveira <jeffolivera01@gmail.com>
2021-06-16 16:50:49 -0300

    Adicionando códigos do projeto

---
+++ GamePy/game.py
@@ -0,0 +1,30 @@
+from models.calcular import Calcular
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+def jogar(pontos: int) -> None:
+    dificuldade: int = int(input('Informe o nível de dificuldade desejado (1, 2, 3 ou 4): '))
+
+    calc: Calcular = Calcular(dificuldade)
+
+    print('Informe o resultado para a seguinte operação: ')
+    calc.mostrar_operacao() # 5 + 4 =?
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Você tem {pontos} ponto(s).')
+
+    continuar: int = int(input('Deseja continuar no jogo? (Digite 1 para SIM, e 0 para NÃO): '))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'Você finalizou com {pontos} ponto(s).')
+        print('Até a próxima')
+
+if __name__ == '__main__':
+    main()
